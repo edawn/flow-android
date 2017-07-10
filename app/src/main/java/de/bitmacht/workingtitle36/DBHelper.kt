2940ffbc1a4caf8971a0c8e0e02a0f1c12ed60d3
Kamil Sartys <kamil@kugelhaufen.de>
2017-07-10 17:07:36 +0200

    Kotlinize DBHelper

--- app/src/main/java/de/bitmacht/workingtitle36/DBHelper.kt
+++ app/src/main/java/de/bitmacht/workingtitle36/DBHelper.kt
@@ -17,12 +17,10 @@
 package de.bitmacht.workingtitle36
 
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.os.Build
-
-import java.util.ArrayList
+import java.util.*
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, DBHelper.DATABASE_NAME, null, DBHelper.DATABASE_VERSION) {
 
@@ -44,146 +42,135 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DBHelper.DATABASE_N
 
     companion object {
 
-        val DATABASE_NAME = "flow"
-        val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "flow"
+        const val DATABASE_VERSION = 1
 
         /**
          * This table contains all transactions.
          */
-        val TRANSACTIONS_TABLE_NAME = "transactions"
+        const val TRANSACTIONS_TABLE_NAME = "transactions"
         /**
          * The id of this transaction
          */
-        val TRANSACTIONS_KEY_ID = "id"
+        const val TRANSACTIONS_KEY_ID = "id"
         /**
          * Indicates that this transaction has been removed and should not be processed any further
          */
-        val TRANSACTIONS_KEY_IS_REMOVED = "is_removed"
+        const val TRANSACTIONS_KEY_IS_REMOVED = "is_removed"
 
-        val TRANSACTIONS_TABLE_CREATE =
-                "CREATE TABLE " + TRANSACTIONS_TABLE_NAME + "(" +
-                        TRANSACTIONS_KEY_ID + " INTEGER PRIMARY KEY, " +
-                        TRANSACTIONS_KEY_IS_REMOVED + " BOOLEAN" +
-                        ");"
+        const val TRANSACTIONS_TABLE_CREATE =
+                "CREATE TABLE $TRANSACTIONS_TABLE_NAME($TRANSACTIONS_KEY_ID INTEGER PRIMARY KEY, $TRANSACTIONS_KEY_IS_REMOVED BOOLEAN);"
 
         /**
          * This table contains all edits. Once created, an edit is never modified.
          * Additionally an edit is never removed, unless the transaction and all edits belonging to it are removed as well.
          */
-        val EDITS_TABLE_NAME = "edits"
+        const val EDITS_TABLE_NAME = "edits"
         /**
          * The id of this edit
          */
-        val EDITS_KEY_ID = "id"
+        const val EDITS_KEY_ID = "id"
         /**
          * The id of the parent that this entry has been derived from;
          * if this is the first edit in a transaction then this is NULL
          */
-        val EDITS_KEY_PARENT = "parent"
+        const val EDITS_KEY_PARENT = "parent"
         /**
          * The id of the transaction that this edit belongs to;
          * the referenced transaction must exist
          * Note: "transaction" is an sqlite keyword
          */
-        val EDITS_KEY_TRANSACTION = "transaktion"
+        const val EDITS_KEY_TRANSACTION = "transaktion"
         /**
          * This identifies an edit in a transaction; for the first edit this is zero;
          * a new edit gets the highest sequence number of any edit in the related transaction, incremented by one;
          * the edit with the highest sequence number which is not pending is used for the calculation of the flow
          */
-        val EDITS_KEY_SEQUENCE = "sequence"
+        const val EDITS_KEY_SEQUENCE = "sequence"
         /**
          * Indicate that the user has not finished editing this entry; this may happen when the app is put
          * in the background before the user clicks "accept" or the like
          */
-        val EDITS_KEY_IS_PENDING = "is_pending"
+        const val EDITS_KEY_IS_PENDING = "is_pending"
         /**
          * The user-set time of the transaction; in ms since the epoch
          */
-        val EDITS_KEY_TRANSACTION_TIME = "transaction_time"
+        const val EDITS_KEY_TRANSACTION_TIME = "transaction_time"
         /**
          * The user-set description for this transaction
          */
-        val EDITS_KEY_TRANSACTION_DESCRIPTION = "transaction_description"
+        const val EDITS_KEY_TRANSACTION_DESCRIPTION = "transaction_description"
         /**
          * The user-set location that this transaction took place;
          * preferably as geo URI (https://en.wikipedia.org/wiki/Geo_URI_scheme)
          */
-        val EDITS_KEY_TRANSACTION_LOCATION = "transaction_location"
+        const val EDITS_KEY_TRANSACTION_LOCATION = "transaction_location"
         /**
          * The user-set currency for this transaction; a ISO 4217 currency code
          */
-        val EDITS_KEY_TRANSACTION_CURRENCY = "transaction_currency"
+        const val EDITS_KEY_TRANSACTION_CURRENCY = "transaction_currency"
         /**
          * The user-set transaction amount in minor currency units (usually cents); negative values indicate spending
          */
-        val EDITS_KEY_TRANSACTION_AMOUNT = "transaction_amount"
+        const val EDITS_KEY_TRANSACTION_AMOUNT = "transaction_amount"
 
-        val EDITS_TABLE_CREATE =
-                "CREATE TABLE " + EDITS_TABLE_NAME + "(" +
-                        EDITS_KEY_ID + " INTEGER PRIMARY KEY, " +
-                        EDITS_KEY_PARENT + " INTEGER REFERENCES " + EDITS_TABLE_NAME + ", " +
-                        EDITS_KEY_TRANSACTION + " INTEGER, " +
-                        EDITS_KEY_SEQUENCE + " INTEGER, " +
-                        EDITS_KEY_IS_PENDING + " BOOLEAN, " +
-                        EDITS_KEY_TRANSACTION_TIME + " INTEGER, " +
-                        EDITS_KEY_TRANSACTION_DESCRIPTION + " TEXT, " +
-                        EDITS_KEY_TRANSACTION_LOCATION + " TEXT, " +
-                        EDITS_KEY_TRANSACTION_CURRENCY + " TEXT, " +
-                        EDITS_KEY_TRANSACTION_AMOUNT + " INTEGER, " +
-                        "FOREIGN KEY(" + EDITS_KEY_TRANSACTION + ") REFERENCES " + TRANSACTIONS_TABLE_NAME + "(" + TRANSACTIONS_KEY_ID + "), " +
-                        "UNIQUE(" + EDITS_KEY_TRANSACTION + ", " + EDITS_KEY_SEQUENCE + ")" +
-                        ");"
+        const val EDITS_TABLE_CREATE =
+                "CREATE TABLE $EDITS_TABLE_NAME($EDITS_KEY_ID INTEGER PRIMARY KEY, $EDITS_KEY_PARENT INTEGER REFERENCES $EDITS_TABLE_NAME, " +
+                        "$EDITS_KEY_TRANSACTION INTEGER, $EDITS_KEY_SEQUENCE INTEGER, $EDITS_KEY_IS_PENDING BOOLEAN, " +
+                        "$EDITS_KEY_TRANSACTION_TIME INTEGER, $EDITS_KEY_TRANSACTION_DESCRIPTION TEXT, $EDITS_KEY_TRANSACTION_LOCATION TEXT, " +
+                        "$EDITS_KEY_TRANSACTION_CURRENCY TEXT, $EDITS_KEY_TRANSACTION_AMOUNT INTEGER, " +
+                        "FOREIGN KEY($EDITS_KEY_TRANSACTION) REFERENCES $TRANSACTIONS_TABLE_NAME($TRANSACTIONS_KEY_ID), " +
+                        "UNIQUE($EDITS_KEY_TRANSACTION, $EDITS_KEY_SEQUENCE));"
 
         /**
          * This table contains the regular transactions
          */
-        val REGULARS_TABLE_NAME = "regulars"
+        const val REGULARS_TABLE_NAME = "regulars"
 
         /**
          * The id of this transaction
          */
-        val REGULARS_KEY_ID = "id"
+        const val REGULARS_KEY_ID = "id"
         /**
          * The time this transaction will be executed for the first time;
          * in ms since the epoch; including
          */
-        val REGULARS_KEY_TIME_FIRST = "time_first"
+        const val REGULARS_KEY_TIME_FIRST = "time_first"
         /**
          * The time this transaction will be executed for the last time;
          * in ms since the epoch; excluding; a negative value indicates that this transaction will happen indefinitely
          */
-        val REGULARS_KEY_TIME_LAST = "time_last"
+        const val REGULARS_KEY_TIME_LAST = "time_last"
         /**
          * Defines the way this transaction is supposed to repeat;
          * 0: daily 1: monthly
          */
-        val REGULARS_KEY_PERIOD_TYPE = "period_type"
+        const val REGULARS_KEY_PERIOD_TYPE = "period_type"
         /**
          * A multiplier for the period; useful to create weekly, quarterly or yearly expenses for instance
          */
-        val REGULARS_KEY_PERIOD_MULTIPLIER = "period_multiplier"
+        const val REGULARS_KEY_PERIOD_MULTIPLIER = "period_multiplier"
         /**
          * Spread this transaction across multiple accounting periods (this makes only sense
          * if it overlays multiple accounting periods)
          */
-        val REGULARS_KEY_IS_SPREAD = "is_spread"
+        const val REGULARS_KEY_IS_SPREAD = "is_spread"
         /**
          * Do not use this transaction when calculating the balance
          */
-        val REGULARS_KEY_IS_DISABLED = "is_disabled"
+        const val REGULARS_KEY_IS_DISABLED = "is_disabled"
         /**
          * The user-set description for this transaction
          */
-        val REGULARS_KEY_DESCRIPTION = "description"
+        const val REGULARS_KEY_DESCRIPTION = "description"
         /**
          * The user-set currency for this transaction; a ISO 4217 currency code
          */
-        val REGULARS_KEY_CURRENCY = "currency"
+        const val REGULARS_KEY_CURRENCY = "currency"
         /**
          * The user-set transaction amount; in minor currency units (usually cents); negative values indicate spending
          */
-        val REGULARS_KEY_AMOUNT = "amount"
+        const val REGULARS_KEY_AMOUNT = "amount"
 
         /**
          * Relates to [DBHelper.REGULARS_KEY_PERIOD_TYPE]; this transactions will be executed every day
@@ -194,23 +181,16 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DBHelper.DATABASE_N
          */
         const val REGULARS_PERIOD_TYPE_MONTHLY = 1
 
-        val REGULARS_TABLE_CREATE =
-                "CREATE TABLE " + REGULARS_TABLE_NAME + "(" +
-                        REGULARS_KEY_ID + " INTEGER PRIMARY KEY, " +
-                        REGULARS_KEY_TIME_FIRST + " INTEGER, " +
-                        REGULARS_KEY_TIME_LAST + " INTEGER, " +
-                        REGULARS_KEY_PERIOD_TYPE + " INTEGER, " +
-                        REGULARS_KEY_PERIOD_MULTIPLIER + " INTEGER, " +
-                        REGULARS_KEY_IS_SPREAD + " BOOLEAN, " +
-                        REGULARS_KEY_IS_DISABLED + " BOOLEAN, " +
-                        REGULARS_KEY_DESCRIPTION + " TEXT, " +
-                        REGULARS_KEY_CURRENCY + " TEXT, " +
-                        REGULARS_KEY_AMOUNT + " INTEGER);"
+        const val REGULARS_TABLE_CREATE =
+                "CREATE TABLE $REGULARS_TABLE_NAME($REGULARS_KEY_ID INTEGER PRIMARY KEY, $REGULARS_KEY_TIME_FIRST INTEGER, " +
+                        "$REGULARS_KEY_TIME_LAST INTEGER, $REGULARS_KEY_PERIOD_TYPE INTEGER, $REGULARS_KEY_PERIOD_MULTIPLIER INTEGER, " +
+                        "$REGULARS_KEY_IS_SPREAD BOOLEAN, $REGULARS_KEY_IS_DISABLED BOOLEAN, $REGULARS_KEY_DESCRIPTION TEXT, " +
+                        "$REGULARS_KEY_CURRENCY TEXT, $REGULARS_KEY_AMOUNT INTEGER);"
 
         /**
          * Returns the most current, not pending edit for every transaction
          */
-        val LATEST_EDITS_QUERY =
+        const val LATEST_EDITS_QUERY =
                 "SELECT " + EDITS_TABLE_NAME + ".* FROM " + EDITS_TABLE_NAME + " INNER JOIN " +
                         "(SELECT " + EDITS_KEY_ID + ", MAX(" + EDITS_KEY_SEQUENCE + ") AS maxsequence " +
                         "FROM " + EDITS_TABLE_NAME + " WHERE NOT " + EDITS_KEY_IS_PENDING + " GROUP BY " +
@@ -220,77 +200,66 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, DBHelper.DATABASE_N
         /**
          * Returns the most current, not removed transaction/edit join for every transaction
          */
-        val TRANSACTIONS_EDITS_QUERY =
-                "SELECT " + EDITS_TABLE_NAME + ".*, " + TRANSACTIONS_KEY_IS_REMOVED + " FROM " + EDITS_TABLE_NAME + " INNER JOIN " +
-                        "(SELECT " + TRANSACTIONS_KEY_IS_REMOVED +
-                        ", " + EDITS_TABLE_NAME + "." + EDITS_KEY_ID + ", MAX(" + EDITS_KEY_SEQUENCE + ") AS maxsequence " +
-                        "FROM " + TRANSACTIONS_TABLE_NAME + ", " + EDITS_TABLE_NAME + " WHERE " + TRANSACTIONS_TABLE_NAME + "." + TRANSACTIONS_KEY_ID +
-                        " = " + EDITS_KEY_TRANSACTION + " AND NOT " + TRANSACTIONS_KEY_IS_REMOVED + " GROUP BY " + EDITS_KEY_TRANSACTION + ") editsmax ON " +
-                        EDITS_TABLE_NAME + "." + EDITS_KEY_ID + " = editsmax." + EDITS_KEY_ID +
-                        " ORDER BY " + EDITS_KEY_TRANSACTION_TIME
+        const val TRANSACTIONS_EDITS_QUERY =
+                "SELECT $EDITS_TABLE_NAME.*, $TRANSACTIONS_KEY_IS_REMOVED FROM $EDITS_TABLE_NAME INNER JOIN (SELECT $TRANSACTIONS_KEY_IS_REMOVED, " +
+                        "$EDITS_TABLE_NAME.$EDITS_KEY_ID, MAX($EDITS_KEY_SEQUENCE) AS maxsequence FROM $TRANSACTIONS_TABLE_NAME, " +
+                        "$EDITS_TABLE_NAME WHERE $TRANSACTIONS_TABLE_NAME.$TRANSACTIONS_KEY_ID = $EDITS_KEY_TRANSACTION AND NOT " +
+                        "$TRANSACTIONS_KEY_IS_REMOVED GROUP BY $EDITS_KEY_TRANSACTION) editsmax ON " +
+                        "$EDITS_TABLE_NAME.$EDITS_KEY_ID = editsmax.$EDITS_KEY_ID ORDER BY $EDITS_KEY_TRANSACTION_TIME"
 
         /**
          * Returns the most current, not removed transaction/edit join for every transaction in a defined time span
          */
-        val TRANSACTIONS_EDITS_TIME_SPAN_QUERY =
-                "SELECT " + EDITS_TABLE_NAME + ".*, " + TRANSACTIONS_KEY_IS_REMOVED + " FROM " + EDITS_TABLE_NAME + " INNER JOIN " +
-                        "(SELECT " + TRANSACTIONS_KEY_IS_REMOVED +
-                        ", " + EDITS_TABLE_NAME + "." + EDITS_KEY_ID + ", MAX(" + EDITS_KEY_SEQUENCE + ") AS maxsequence " +
-                        "FROM " + TRANSACTIONS_TABLE_NAME + ", " + EDITS_TABLE_NAME + " WHERE " + TRANSACTIONS_TABLE_NAME + "." + TRANSACTIONS_KEY_ID +
-                        " = " + EDITS_KEY_TRANSACTION + " AND NOT " + TRANSACTIONS_KEY_IS_REMOVED +
-                        " AND " + EDITS_KEY_TRANSACTION_TIME + " >= ? AND " + EDITS_KEY_TRANSACTION_TIME + " < ? " +
-                        " GROUP BY " + EDITS_KEY_TRANSACTION + ") editsmax ON " +
-                        EDITS_TABLE_NAME + "." + EDITS_KEY_ID + " = editsmax." + EDITS_KEY_ID +
-                        " ORDER BY " + EDITS_KEY_TRANSACTION_TIME
+        const val TRANSACTIONS_EDITS_TIME_SPAN_QUERY =
+                "SELECT $EDITS_TABLE_NAME.*, $TRANSACTIONS_KEY_IS_REMOVED FROM $EDITS_TABLE_NAME INNER JOIN " +
+                        "(SELECT $TRANSACTIONS_KEY_IS_REMOVED, $EDITS_TABLE_NAME.$EDITS_KEY_ID, MAX($EDITS_KEY_SEQUENCE) AS maxsequence FROM " +
+                        "$TRANSACTIONS_TABLE_NAME, $EDITS_TABLE_NAME WHERE $TRANSACTIONS_TABLE_NAME.$TRANSACTIONS_KEY_ID = $EDITS_KEY_TRANSACTION AND NOT " +
+                        "$TRANSACTIONS_KEY_IS_REMOVED AND $EDITS_KEY_TRANSACTION_TIME >= ? AND $EDITS_KEY_TRANSACTION_TIME < ? GROUP BY $EDITS_KEY_TRANSACTION) " +
+                        "editsmax ON $EDITS_TABLE_NAME.$EDITS_KEY_ID = editsmax.$EDITS_KEY_ID ORDER BY $EDITS_KEY_TRANSACTION_TIME"
 
         /**
          * Query a transaction by id
          */
-        val TRANSACTION_QUERY =
+        const val TRANSACTION_QUERY =
                 "SELECT * FROM $TRANSACTIONS_TABLE_NAME WHERE $TRANSACTIONS_KEY_ID = ?"
 
         /**
          * Returns distinct fields and their frequency in the given column selected from
          * the most current, not pending edit for every transaction
          */
-        val SUGGESTIONS_QUERY =
-                "SELECT TRIM(%s) AS result, COUNT(*) FROM " + EDITS_TABLE_NAME + " INNER JOIN " +
-                        "(SELECT " + EDITS_KEY_ID + ", MAX(" + EDITS_KEY_SEQUENCE + ") AS maxsequence " +
-                        "FROM " + EDITS_TABLE_NAME + " WHERE NOT " + EDITS_KEY_IS_PENDING + " GROUP BY " +
-                        EDITS_KEY_TRANSACTION + ") editsmax ON " + EDITS_TABLE_NAME + "." + EDITS_KEY_ID +
-                        " = editsmax." + EDITS_KEY_ID + " GROUP BY result ORDER BY result"
+        const val SUGGESTIONS_QUERY =
+                "SELECT TRIM(%s) AS result, COUNT(*) FROM $EDITS_TABLE_NAME INNER JOIN (SELECT $EDITS_KEY_ID, MAX($EDITS_KEY_SEQUENCE) AS " +
+                        "maxsequence FROM $EDITS_TABLE_NAME WHERE NOT $EDITS_KEY_IS_PENDING GROUP BY $EDITS_KEY_TRANSACTION) editsmax ON " +
+                        "$EDITS_TABLE_NAME.$EDITS_KEY_ID = editsmax.$EDITS_KEY_ID GROUP BY result ORDER BY result"
 
         /**
          * Returns all edits for a transaction (selected by the id of its associated transaction id)
          */
-        val EDITS_FOR_TRANSACTION_QUERY =
+        const val EDITS_FOR_TRANSACTION_QUERY =
                 "SELECT * FROM $EDITS_TABLE_NAME WHERE $EDITS_KEY_TRANSACTION = ?"
 
         /**
          * Returns all regular transactions
          */
-        val REGULARS_QUERY = "SELECT * FROM " + REGULARS_TABLE_NAME
+        const val REGULARS_QUERY = "SELECT * FROM $REGULARS_TABLE_NAME"
 
         /**
          * Returns all active regular transactions
          */
-        val ACTIVE_REGULARS_QUERY =
+        const val ACTIVE_REGULARS_QUERY =
                 "SELECT * FROM $REGULARS_TABLE_NAME WHERE NOT $REGULARS_KEY_IS_DISABLED"
 
         /**
          * Query the database for regular transactions
          * @see DBHelper.ACTIVE_REGULARS_QUERY
          */
-        @JvmOverloads fun queryRegulars(dbHelper: DBHelper, includeDisabled: Boolean = false): ArrayList<RegularModel> {
-            val db = dbHelper.readableDatabase
-            val cursor = db.rawQuery(if (includeDisabled) REGULARS_QUERY else ACTIVE_REGULARS_QUERY, null)
-
-            val regulars = ArrayList<RegularModel>(cursor.count)
-            while (cursor.moveToNext()) {
-                regulars.add(RegularModel(cursor))
-            }
-            cursor.close()
-            return regulars
-        }
+        fun queryRegulars(dbHelper: DBHelper, includeDisabled: Boolean = false): ArrayList<RegularModel> =
+                dbHelper.readableDatabase.rawQuery(if (includeDisabled) REGULARS_QUERY else ACTIVE_REGULARS_QUERY, null)
+                        .use { cursor ->
+                            with(ArrayList<RegularModel>(cursor.count)) {
+                                while (cursor.moveToNext()) add(RegularModel(cursor))
+                                this
+                            }
+                        }
     }
 }
