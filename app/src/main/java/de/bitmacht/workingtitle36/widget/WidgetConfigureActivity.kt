6e741885131684a1851ea77765d2362d12375193
Kamil Sartys <kamil@kugelhaufen.de>
2017-07-13 19:57:30 +0200

    Kotlinize WidgetConfigureActivity

--- app/src/main/java/de/bitmacht/workingtitle36/widget/WidgetConfigureActivity.kt
+++ app/src/main/java/de/bitmacht/workingtitle36/widget/WidgetConfigureActivity.kt
@@ -16,20 +16,18 @@
 
 package de.bitmacht.workingtitle36.widget
 
+import android.app.Activity
 import android.app.Dialog
 import android.appwidget.AppWidgetManager
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.app.AppCompatDialogFragment
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.SeekBar
-
 import de.bitmacht.workingtitle36.R
 import de.bitmacht.workingtitle36.Utils
 
@@ -37,52 +35,33 @@ class WidgetConfigureActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setResult(RESULT_CANCELED)
-
-        val fragment = WidgetConfigureDialogFragment()
-        fragment.show(supportFragmentManager, "dialog")
+        WidgetConfigureDialogFragment().show(supportFragmentManager, "dialog")
     }
 
     class WidgetConfigureDialogFragment : AppCompatDialogFragment() {
 
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            val contentView = LayoutInflater.from(activity).inflate(R.layout.dialog_widget_configure, null) as LinearLayout
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+                AlertDialog.Builder(activity).setTitle(getString(R.string.widget_config_title, getString(R.string.app_name)))
+                        .setView(LayoutInflater.from(activity).inflate(R.layout.dialog_widget_configure, null) as LinearLayout)
+                        .setPositiveButton(android.R.string.ok, { _, _ ->
+                            val alpha = with(dialog.findViewById(R.id.transparency_seekbar) as SeekBar) { progress.toFloat() / max }
+                            Utils.setfPref(context, R.string.pref_widget_transparency_key, alpha)
 
-            val appName = getString(R.string.app_name)
+                            context.startService(Intent(context, WidgetService::class.java))
 
-            val builder = AlertDialog.Builder(activity)
-            builder.setTitle(getString(R.string.widget_config_title, appName)).setView(contentView)
-
-            builder.setPositiveButton(android.R.string.ok) { dialog, which ->
-                val context = context
-                val seekbar = getDialog().findViewById(R.id.transparency_seekbar) as SeekBar
-                val alpha = seekbar.progress.toFloat() / seekbar.max
-                Utils.setfPref(context, R.string.pref_widget_transparency_key, alpha)
-
-                context.startService(Intent(context, WidgetService::class.java))
-
-                val activity = activity
-                val intent = activity.intent
-                val extras = intent.extras
-                if (extras != null) {
-                    val appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
-                    val resultValue = Intent()
-                    resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-                    activity.setResult(RESULT_OK, resultValue)
-                }
-
-                dismiss()
-            }
-
-            builder.setNegativeButton(android.R.string.cancel) { dialog, which -> dismiss() }
-
-            return builder.create()
-        }
+                            activity.intent.extras?.let { extras ->
+                                activity.setResult(Activity.RESULT_OK, Intent().apply {
+                                    putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                                            extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID))
+                                })
+                            }
+                            dismiss()
+                        })
+                        .setNegativeButton(android.R.string.cancel, { _, _ -> dismiss() }).create()
 
         override fun onDismiss(dialog: DialogInterface?) {
             super.onDismiss(dialog)
-            val activity = activity
             activity?.finish()
         }
     }
